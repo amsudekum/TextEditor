f77eaf7b8045b3b563dd382abe65b0e893c9c3b9
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-08 00:32:12 -0500

    having trouble starting this

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-  const jatetDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.get(1);
